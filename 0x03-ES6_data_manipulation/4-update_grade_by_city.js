55d9196c182e586ee3ae2777c45ac1d3a59823e8
VicvekSr <Olamideoso65@gmail.com>
2023-01-04 03:23:08 +0000

    add a new set array and eslint fixes

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,5 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    return students.filter((student) => student.location === city)
+  return students.filter((student) => student.location === city)
     .map((student) => {
       const stGrade = newGrades.filter((std) => std.studentId === student.id);
       const grade = stGrade.length > 0 ? stGrade[0].grade : 'N/A';
