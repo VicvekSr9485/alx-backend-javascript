6f396df4927fb37da705fd4071a828aeb15ae2c6
VicvekSr <Olamideoso65@gmail.com>
2023-02-20 21:34:44 +0000

    Use Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,8 +2,8 @@ process.stdin.setEncoding('utf8');
 
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (data) => {
-  const name = data.trim();
+process.stdin.on('data', (input) => {
+  const name = input.trim();
   console.log(`Your name is: ${name}`);
   process.exit();
 });
