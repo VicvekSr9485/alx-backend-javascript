2cd746446cd432d7c599e7cb1a247623ba07d0f2
VicvekSr <Olamideoso65@gmail.com>
2023-02-21 10:45:25 +0000

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdin.setEncoding('utf8');
 
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', function (input) {
+process.stdin.on('data', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
   process.exit();
