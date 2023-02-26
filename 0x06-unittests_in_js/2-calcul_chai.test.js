d3edf57f1ebecb297bec62dcfde42c9f290b5efc
VicvekSr <Olamideoso65@gmail.com>
2023-02-26 16:51:09 +0000

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,60 +1,30 @@
-const assert = require('assert');
 const { expect } = require('chai');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
-describe('calculateNumber', function() {
-    describe('when type is SUM', function() {
-        it('should return the sum of two integers', function() {
-            expect(calculateNumber('SUM', 1, 2)).to.equal(3);
-            expect(calculateNumber('SUM', 4, 3)).to.equal(7);
-            expect(calculateNumber('SUM', 0, 0)).to.equal(0);
-        });
+describe('calculateNumber', () => {
+  describe('when type is SUM', () => {
+    it('should round the input values and return the sum', () => {
+      expect(calculateNumber('SUM', 1.5, 2.4)).to.equal(4);
+      expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+    });
+  });
 
-        it('should round the input values', function() {
-            expect(calculateNumber('SUM', 1.5, 2.6)).to.equal(5);
-            expect(calculateNumber('SUM', 4.2, 3.8)).to.equal(8);
-            expect(calculateNumber('SUM', 0.3, 0.6)).to.equal(1);
-          });
+  describe('when type is SUBTRACT', () => {
+    it('should round the input values and return the difference', () => {
+      expect(calculateNumber('SUBTRACT', 5.6, 2.8)).to.equal(3);
+      expect(calculateNumber('SUBTRACT', 3.1, 1.5)).to.equal(1);
+    });
+  });
+
+  describe('when type is DIVIDE', () => {
+    it('should round the input values and return the quotient', () => {
+      expect(calculateNumber('DIVIDE', 10, 3)).to.equal(3.3333333333333335);
+      expect(calculateNumber('DIVIDE', 8, 2)).to.equal(4);
     });
 
-    describe('when type is SUBTRACT', function() {
-        it('should return the difference between two integers', function() {
-          expect(calculateNumber('SUBTRACT', 5, 2)).to.equal(3);
-          expect(calculateNumber('SUBTRACT', 4, 5)).to.equal(-1);
-          expect(calculateNumber('SUBTRACT', 0, 0)).to.equal(0);
-        });
-    
-        it('should round the input values', function() {
-          expect(calculateNumber('SUBTRACT', 2.6, 1.5)).to.equal(1);
-          expect(calculateNumber('SUBTRACT', 4.2, 3.8)).to.equal(0);
-          expect(calculateNumber('SUBTRACT', 0.6, 0.3)).to.equal(1);
-        });
+    it('should return "Error" when b is rounded to 0', () => {
+      expect(calculateNumber('DIVIDE', 10, 0)).to.equal('Error');
+      expect(calculateNumber('DIVIDE', 5, 0)).to.equal('Error');
     });
-
-    describe('when type is DIVIDE', function() {
-        it('should return the quotient of two integers', function() {
-          expect(calculateNumber('DIVIDE', 6, 2)).to.equal(3);
-          expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
-        });
-    
-        it('should round the input values', function() {
-          expect(calculateNumber('DIVIDE', 3.7, 1.5)).to.equal(2);
-          expect(calculateNumber('DIVIDE', 4.2, 3.8)).to.equal(1);
-          expect(calculateNumber('DIVIDE', 1, 3)).to.equal(0.3333333333333333);
-        });
-    
-        it('should return "Error" if b is rounded to 0', function() {
-          expect(calculateNumber('DIVIDE', 1, 0.01)).to.equal('Error');
-          expect(calculateNumber('DIVIDE', 1, 0)).to.equal('Error');
-        });
-    });
-
-    describe('when type is invalid', function() {
-        it('should throw an error', function() {
-          assert.throws(() => calculateNumber('invalid', 1, 2), {
-            name: 'Error',
-            message: 'Invalid type: invalid'
-          });
-        });
-    });
+  });
 });
