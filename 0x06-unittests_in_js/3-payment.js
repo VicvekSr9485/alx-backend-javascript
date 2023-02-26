23acc11aca55960a01bba28407058619033c865f
VicvekSr <Olamideoso65@gmail.com>
2023-02-26 17:34:53 +0000

    add sinon Spies

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
 const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    const sum = Utils.calculateNumber(Utils.SUM, totalAmount, totalShipping);
-    console.log(`The total is: ${sum}`);
+  const sum = Utils.calculateNumber(Utils.SUM, totalAmount, totalShipping);
+  console.log(`The total is: ${sum}`);
 }
 
 module.exports = sendPaymentRequestToApi;
