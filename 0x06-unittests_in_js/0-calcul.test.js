6426715f4c8d6ee36027ed7435d537666d7dca0a
VicvekSr <Olamideoso65@gmail.com>
2023-02-26 15:37:52 +0000

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -13,4 +13,16 @@ describe('calculateNumber', function () {
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
+
+    it('should round the first input argument', function() {
+        assert.strictEqual(calculateNumber(1.4, 2), 3);
+        assert.strictEqual(calculateNumber(2.5, 3), 6);
+        assert.strictEqual(calculateNumber(1.6, 1), 3);
+      });
+
+    it('should round the second input argument', function() {
+        assert.strictEqual(calculateNumber(1, 2.4), 3);
+        assert.strictEqual(calculateNumber(4, 3.8), 8);
+        assert.strictEqual(calculateNumber(0, 1.5), 2);
+      });
 });
