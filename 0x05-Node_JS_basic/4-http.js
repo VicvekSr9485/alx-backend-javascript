792ae88e55d0be1249d39b5191758d1a9649f642
VicvekSr <Olamideoso65@gmail.com>
2023-02-26 01:55:11 +0000

    eslint fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,13 +4,13 @@ const hostName = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end('Hello Holberton School!');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostName, () => {
-    console.log(`Server running at http://${hostName}:${port}/`);
-  });
+  console.log(`Server running at http://${hostName}:${port}/`);
+});
 
 module.exports = app;
