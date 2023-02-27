a65d29dd5ca69198b89e8146b38ce6a28e1b6186
VicvekSr <Olamideoso65@gmail.com>
2023-02-27 09:23:53 +0000

    Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,13 +3,23 @@ const request = require('supertest');
 const { expect } = require('chai');
 
 describe('test GET /', () => {
-    it('GET / should return 200 status code', async () => {
-        const response = await request(app).get('/');
-        expect(response.statusCode).to.equal(200);
+    it('GET / should return 200 status code', (done) => {
+        request(app)
+            .get('/')
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                done();
+            });
     });
     
-    it('GET / should return Welcome to the payment system', async () => {
-        const response = await request(app).get('/');
-        expect(response.text).to.equal('Welcome to the payment system');
+    it('GET / should return Welcome to the payment system', (done) => {
+        request(app)
+            .get('/')
+            .expect('Welcome to the payment system')
+            .end((err, res) => {
+                if (err) return done(err);
+                done();
+            });
     });
 });
